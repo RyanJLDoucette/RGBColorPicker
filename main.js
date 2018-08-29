e8952f0215e67c55d5fac70b2cc7b5de60447f20
Ryan Doucette <31770773+RyanJLDoucette@users.noreply.github.com>
2018-08-29 20:03:30 -0300

    Functionality to easy and hard buttons
    Made it so if you click the easy button and it is currently in hard mode, then it will start a new easy game. IF it's already in easy mode then it won't do anything (if you wanted new easy game hit new colors). And the same functionality is done to the hard button too. One thing I have not done is hide the last 3 colors if in easy mode.

--- main.js
+++ main.js
@@ -9,32 +9,60 @@ var redValue = document.querySelector("#red_value");
 var greenValue = document.querySelector("#green_value");
 var blueValue = document.querySelector("#blue_value");
 var newGameButton = document.querySelector("#new_colors");
+var easyModeButton = document.querySelector("#easy");
+var hardModeButton = document.querySelector("#hard");
 var labelList = [p1,p2,p3,p4,p5,p6];
+var totalColors = 6;//Two game modes. Easy and hard. Easy has 3 colors, and hard has 6 colors. Hard is default, but if easy button is clicked, then the number of colors to choose from goes to 3
 
 
-var colors = getRandomColors();//Colors contains a list of arrays of length 3. The array represent an rgb color.
-var winningColor = colors[getWinningNumber(6)];//winning number picks which of the 3 or 6 (depends on easy or hard) is the winning number
+var colors = getRandomColors(totalColors);//Colors contains a list of arrays of length 3. The array represent an rgb color.
+var winningColor = colors[getWinningNumber(totalColors)];//winning number picks which of the 3 or 6 (depends on easy or hard) is the winning number
 redValue.textContent = winningColor[0];//since winner color is an array of 3 integers. The 'r' in rgb is the first value of the array
 greenValue.textContent = winningColor[1];
 blueValue.textContent = winningColor[2];
-setColors(colors);//visually sets the colors
+setColors(colors, totalColors);//visually sets the colors
 
 
+//if the game mode is already easy. Then this button should not do anything, it should be already disabled. Otherwise start a new easy game
+easyModeButton.addEventListener("click", function() {
+    if(totalColors == 3) {
+        //do nothing
+    }
+    else {
+        //start a new game
+        totalColors = 3;
+        newGame();
+    }
+});
 
+//if the game mode is already hard. Then this button should not do anything, it should be already disabled. Otherwise start a new hard game
+hardModeButton.addEventListener("click", function() {
+    if(totalColors == 6) {
+        //do nothing
+    }
+    else {
+        //start a new game
+        totalColors = 6;
+        newGame();
+    }
+});
 
-newGameButton.addEventListener("click", function(){
-    colors = getRandomColors();
-    winningColor = colors[getWinningNumber(6)];
+newGameButton.addEventListener("click", newGame);
+
+function newGame() {
+    colors = getRandomColors(totalColors);
+    console.log(totalColors);
+    winningColor = colors[getWinningNumber(totalColors)];
     redValue.textContent = winningColor[0];
     greenValue.textContent = winningColor[1];
     blueValue.textContent = winningColor[2];
-    setColors(colors);//visually sets the colors
-});
+    setColors(colors, totalColors);//visually sets the colors
+}
 
-//Returns a list of colors. The number of colors depends on if easy (0) or hard(1) mode was selected. Each color is an array the the form RGB (0-255,0-255,0-255) inclusive.
-function getRandomColors() {
+//Returns a list of colors. Each color is an array the the form RGB (0-255,0-255,0-255) inclusive.
+function getRandomColors(totalColors) {
     var masterList = [];
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < totalColors; i++) {
         var rgbList = [];
         for (let j = 0; j < 3; j++) {
             rgbList.push(Math.floor(Math.random() * Math.floor(256)));
@@ -46,13 +74,17 @@ function getRandomColors() {
 }
 
 //Sets all the colors to their corresponding values 
-function setColors(rgbList,winningNumber) {
-    this.rgbList = rgbList;
+function setColors(rgbList, totalColors) {
     var color = "";
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < totalColors; i++) {
         var curr = rgbList[i];
         color = "rgb(" + curr.shift() + "," + curr.shift() + "," + curr.shift() +")";
         labelList[i].textContent = color;
+    }
+    if(totalColors == 3) {
+        for(let i = 3; i < 6; i++) {
+            labelList[i].classList.add(".hidden");
+        }
     }
 }
 
